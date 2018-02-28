9e31bfc871df2022e1f5abeb9fef4bd7a5852e69
Marcin Bogdanski <marcin.bogdanski@dronex.co.uk>
2018-02-28 00:48:09 +0000

    KerasApproximator, remove old code, improve asserts

--- rl_agent/agents/approximators/kerasnn.py
+++ rl_agent/agents/approximators/kerasnn.py
@@ -16,8 +16,19 @@ class KerasApproximator:
         self._vel_scale = 1 / 0.07 # 2 / 0.14  # maps vel to -1..1
 
     def set_state_action_spaces(self, state_space, action_space):
+        first_layer = self._model.layers[0]
+        nn_input_shape = first_layer.input_shape[1:]
+        if state_space.shape != nn_input_shape:
+            raise ValueError('Input shape does not match state_space shape')
+
+        last_layer = self._model.layers[-1]
+        nn_output_shape = action_space.shape
+        if action_space.shape != nn_output_shape:
+            raise ValueError('Output shape does not match action_space shape')
+
         self._state_space = state_space
         self._action_space = action_space
+        
 
     def get_weights_fingerprint(self):
         weights_sum = 0
@@ -27,21 +38,14 @@ class KerasApproximator:
             weights_sum += layer_sum
         return weights_sum
 
-    def _test_input(self, state, action):
-        assert isinstance(state, np.ndarray)
-        assert isinstance(state[0], float)
-        assert isinstance(state[1], float)
-        assert isinstance(action, int) or isinstance(action, np.int64)
-        pos, vel = state[0], state[1]
-        assert -1.2 <= pos and pos <= 0.5
-        assert -0.07 <= vel and vel <= 0.07
-
-        return pos, vel, action
 
     def estimate(self, state, action):
         assert self._state_space is not None
         assert self._action_space is not None
-        pos, vel, action = self._test_input(state, action)
+        assert self._state_space.contains(state)
+        assert self._action_space.contains(action)
+        
+        pos, vel = state[0], state[1]
 
         pos += self._pos_offset
         pos *= self._pos_scale
@@ -56,137 +60,53 @@ class KerasApproximator:
         assert self._state_space is not None
         assert self._action_space is not None
         assert isinstance(states, np.ndarray)
-        assert states.ndim == 2
-        assert len(states) > 0
-        assert states.shape[1] == 2   # pos, vel
-        assert states.dtype == np.float32 or states.dtype == np.float64
-        assert np.min(states, axis=0)[0] >= -1.2  # pos
-        assert np.max(states, axis=0)[0] <= 0.5  # pos
-        assert np.min(states, axis=0)[1] >= -0.07  # vel
-        assert np.max(states, axis=0)[1] <= 0.07  # vel
+        assert states.shape[1:] == self._state_space.shape
+        # assert [self._state_space.contains(x) for x in states]
 
-        states = np.array(states)
+        states = np.copy(states)
 
         states[:,0] += self._pos_offset
         states[:,0] *= self._pos_scale
         states[:,1] *= self._vel_scale
 
-        est = self._model.predict(states, batch_size=len(states))
-        return est  # return 2d array
+        return self._model.predict(states, batch_size=len(states))
 
-    def update(self, batch):
-        assert self._state_space is not None
-        assert self._action_space is not None
-        assert isinstance(batch, list)
-        assert len(batch) > 0
-        assert len(batch[0]) == 5
-
-        inputs = []
-        targets = []
-        for tup in batch:
-            St = tup[0]
-            At = tup[1]
-            Rt_1 = tup[2]
-            St_1 = tup[3]
-            done = tup[4]
-
-            pp = St[0]
-            vv = St[1]
-            aa = At
-
-            rr_n = Rt_1
-            pp_n = St_1[0]
-            vv_n = St_1[1]
-
-            pp += self._pos_offset
-            pp *= self._pos_scale
-            vv *= self._vel_scale
-
-            pp_n += self._pos_offset
-            pp_n *= self._pos_scale
-            vv_n *= self._vel_scale
-
-            inp = np.array([[pp, vv]])
-            inp_n = np.array([[pp_n, vv_n]])
-
-            est = self._model.predict(inp, batch_size=len(inp))
-            est_n = self._model.predict(inp_n, batch_size=len(inp_n))
-            q_n = np.max(est_n)
-
-            if done:
-                tt = rr_n 
-            else:
-                tt = rr_n + self._discount * q_n
-
-            assert aa in [0, 1, 2]
-            assert est.shape[0] == 1
-            est[0, aa] = tt
-
-            inputs.append([pp, vv])
-            targets.append(est[0])
-        
-        inputs = np.array(inputs)
-        targets = np.array(targets)
-        
-        self._model.train_on_batch(inputs, targets)
         
     def update2(self, states, actions, rewards_n, states_n, dones):
         assert self._state_space is not None
         assert self._action_space is not None
+
         assert isinstance(states, np.ndarray)
-        assert states.dtype == float
-        assert states.ndim == 2
-        assert states.shape[0] >= 1
+        assert states.shape[1:] == self._state_space.shape
+        # assert all(map(self._state_space.contains, states))
 
         assert isinstance(actions, np.ndarray)
-        assert actions.dtype == int
-        assert (actions.ndim == 2 and actions.shape[0] >= 1 and actions.shape[1] == 1) \
-                or (actions.ndim == 1 and actions.shape[0] >= 1)
+        assert actions.shape[1:] == self._action_space.shape
+        # assert all(map(self._action_space.contains, actions))
 
         assert isinstance(rewards_n, np.ndarray)
         assert rewards_n.dtype == float
         assert rewards_n.ndim == 1
-        assert rewards_n.shape[0] >= 1
 
         assert isinstance(states_n, np.ndarray)
-        assert states_n.dtype == float
-        assert states_n.ndim == 2
-        assert states_n.shape[0] >= 1
+        assert states_n.shape[1:] == self._state_space.shape
+        # assert all(map(self._state_space.contains, states_n))
 
         assert isinstance(dones, np.ndarray)
         assert dones.dtype == bool
         assert dones.ndim == 1
-        assert dones.shape[0] >= 1
+
+        assert len(states) == len(actions) \
+            == len(rewards_n) == len(states_n) == len(dones)
 
 
-
-        # assert len(batch) > 0
-        # assert len(batch[0]) == 5
-
-        # inputs = np.zeros([len(batch), 2], dtype=np.float32)
-        # actions = np.zeros([len(batch)], dtype=np.int8)
-        # rewards_n = np.zeros([len(batch), 1], dtype=np.float32)
-        # inputs_n = np.zeros([len(batch), 2], dtype=np.float32)
-        # not_dones = np.zeros([len(batch), 1], dtype=np.bool)
+        #
+        #   This chunk could use some comments
+        #
         
         inputs = np.array(states)
         inputs_n = np.array(states_n)
         not_dones = np.logical_not(dones)
-        
-        # for i, tup in enumerate(batch):
-        #     St = tup[0]
-        #     At = tup[1]
-        #     Rt_1 = tup[2]
-        #     St_1 = tup[3]
-        #     done = tup[4]
-
-        #     inputs[i] = St
-        #     actions[i] = At
-        #     rewards_n[i] = Rt_1
-        #     inputs_n[i] = St_1
-        #     not_dones[i] = not done
-
-        #     assert At in [0, 1, 2]
         
         inputs[:,0] += self._pos_offset
         inputs[:,0] *= self._pos_scale
