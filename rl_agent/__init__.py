84701800f8460b35a02b8e33dc4ec8659343a613
Marcin Bogdanski <marcin.bogdanski@dronex.co.uk>
2018-03-14 17:32:21 +0000

    Update and tidy agents

--- rl_agent/__init__.py
+++ rl_agent/__init__.py
@@ -1,7 +1,7 @@
 from .agents.runner import train_agent  # function
 from .agents.agent_offline import AgentOffline
-from .agents.sarsa import AgentSARSA
-from .agents.dqn import AgentDQN
+from .agents.agent_sarsa import AgentSARSA
+from .agents.agent_dqn import AgentDQN
 from .agents.memory import Memory
 from .agents.approximators import QFunctTabular
 from .agents.approximators import QFunctAggregate
